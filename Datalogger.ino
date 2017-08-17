3d3ab9b36108108bb262386d916f25fbf0b2bf8d
jeremygilly <mjjgillbanks@hotmail.com>
2017-08-17 16:32:08 +0800

    Updated datalogger.ino
    Removed excess code. Added comments for explanation.

--- Datalogger.ino
+++ Datalogger.ino
@@ -1,22 +1,18 @@
 #include <Statistic.h>
 Statistic myStats;
-//long double initialReading = 0;
 int sourceSensePin = A0;    // select the input pin for the potentiometer
 int drainSensePin = A1;
 double sourceSense = 0;
 double drainSense = 0;
 double sensor = 0;
-int numberOfReadings = 400;
+int numberOfReadings = 400; // change this as required
 unsigned int ADCValue;
 double Vcc;
-double result;
+long result;
 double averagedOutput;
-
-double array[99];
 
 
 long readVcc() {
-  long result;
   // Read 1.1V reference against AVcc
   ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
   delay(2); // Wait for Vref to settle
@@ -38,20 +34,8 @@ double reading() {
   drainSense = (drainSense / 1024.0) * Vcc;
 
   sensor = sourceSense - drainSense;
-  if (sensor == 0) {
-    sensor = reading();
-  }
-//  Serial.println(sensor);
   
   return sensor;
-}
-
-double average (double * array, int len)
-{
-  long sum = 0L ;  // sum will be larger than an item, long for safety.
-  for (int i = 0 ; i < len ; i++)
-    sum += array[i];
-  return  ((double) sum) / len ;  // average will be fractional, so float may be appropriate.
 }
 
 void setup() { 
@@ -61,13 +45,11 @@ void setup() {
 
 void loop() {
 //  int start_time = millis();
-  memset(array,0,sizeof(array));
-  
   for (int i = 0; i < numberOfReadings; i++) {
     double sample_reading = reading();
       myStats.add(sample_reading);
-      // Code takes ~245 ms to run (100 readings per output reading). 
-      //Adding a delay of 1 ms increases run time to 345 ms.
+//    Code takes ~245 ms to run (100 readings per output reading). 
+//    When delay = 0
       delay(0);
   }
   
